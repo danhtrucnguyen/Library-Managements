282cdd77a18bb524af14fa79e27d5b46522c242f
DanhTruc2002 <danhtrucsaison@gmail.com>
2024-11-21 16:05:24 +0700

    edit, delete book

--- src/main/java/com/library/service/impl/BookServiceImpl.java
+++ src/main/java/com/library/service/impl/BookServiceImpl.java
@@ -1,10 +1,17 @@
 package com.library.service.impl;
 
+import java.io.File;
 import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.library.model.Book;
 import com.library.repository.BookRepository;
@@ -28,8 +35,8 @@ public class BookServiceImpl implements BookService {
 	
 	@Override
 	public Book getBookById(Integer id) {
-		Book product = bookRepository.findById(id).orElse(null);
-		return product;
+		Book book = bookRepository.findById(id).orElse(null);
+		return book;
 	}
 	
 	@Override
@@ -42,5 +49,56 @@ public class BookServiceImpl implements BookService {
 		}
 
 		return books;
+	}
+	
+	@Override
+	public Boolean deleteBook(Integer id) {
+		Book book = bookRepository.findById(id).orElse(null);
+
+		if (!ObjectUtils.isEmpty(book)) {
+			bookRepository.delete(book);
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public Book updateBook(Book book, MultipartFile image) {
+
+		Book dbBook = getBookById(book.getId());
+
+		String imageName = image.isEmpty() ? dbBook.getImage() : image.getOriginalFilename();
+
+		dbBook.setBookName(book.getBookName());
+		dbBook.setDescription(book.getDescription());
+		dbBook.setAuthor(book.getAuthor());
+		dbBook.setCategory(book.getCategory());
+		dbBook.setStock(book.getStock());
+		dbBook.setImage(imageName);
+		dbBook.setIsActive(book.getIsActive());
+		dbBook.setIsbn(book.getIsbn());
+
+		
+
+		Book updateProduct = bookRepository.save(dbBook);
+
+		if (!ObjectUtils.isEmpty(updateProduct)) {
+
+			if (!image.isEmpty()) {
+
+				try {
+					File saveFile = new ClassPathResource("static/images").getFile();
+
+					Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "book_img" + File.separator
+							+ image.getOriginalFilename());
+					Files.copy(image.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return book;
+		}
+		return null;
 	}
 }
